85eff121dce7fe026b49dc2ec6a9080cb622655c
Gi <giovammi@protonmail.com>
2022-07-28 20:33:57 -0400

    Hello Concurrent World
    A simple "Hello World" program using an additional thread to print "Hello Concurrent World."

--- multithreading.cpp
+++ multithreading.cpp
@@ -1,6 +1,16 @@
-#include <iostream> 
+// Hello World of Concurrency 
+#include <iostream>
+#include <thread> // 1
+
+void hello()  // 2. Has to have an inital function
+{
+    std::cout << "Hello Concurrent World\n";
+}
 
 int main() 
 {
+    std::thread t(hello); // 3
+    t.join(); // 4. Wait for the thread associated with std::thread object 
+
     return EXIT_SUCCESS;
 }
